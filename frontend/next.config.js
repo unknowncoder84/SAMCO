03ea698f7b075ae0045539e5286581cbd3070b36
unknowncoder84 <unknowncoder84@users.noreply.github.com>
2026-02-22 00:09:02 +0530

    Fix fonts and path aliases for Next.js 14

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
